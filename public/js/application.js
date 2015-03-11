85576a834ffa165535a1521693c857eff7fed78d
Phanendar Chitraju <phanendar@gmail.com>
2015-03-11 16:39:04 -0400

    added more functialility to the dashboard

--- public/js/application.js
+++ public/js/application.js
@@ -19,25 +19,6 @@ $(document).ready(function() {
         console.log("complete");
       });
 
-      //    $.ajax({
-      //   url: "/user_blogs",
-      //   type: "get"
-      // })
-      // .done(function(response) {
-      //   console.log("success:", response)
-      //   $.each(response, function(i) {
-      //     $(".user_blogs").append(response[i].title).wrap("<p></p>")
-      //     $(".user_blogs").append(response[i].body).wrap("<p></p>")
-      //     // console.log(response[i])
-      //   })
-      // })
-
-      // .fail(function(err) {
-      //   console.log("error:", err);
-      // })
-      // .always(function() {
-      //   console.log("complete");
-      // });
 
     $(".login_button").on("click", function(){
         $(".login").show();
