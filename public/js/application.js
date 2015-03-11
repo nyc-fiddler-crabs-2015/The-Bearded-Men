62788f049794900de3ce0625721816e6c80b07fd
Phanendar Chitraju <phanendar@gmail.com>
2015-03-11 10:45:23 -0400

    added navbar, dylogin

---
+++ public/js/application.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+    $(".login_button").on("click", function(){
+        $(".login").show();
+    });
+
+     $(".signup").hide();
+    $(".signup_button").on("click", function(){
+        $(".signup").show();
+    });
+
+
+
+});
